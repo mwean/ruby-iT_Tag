2ba8cc920419bd0491735bb3a83fe3e1c38419c6
Matt Wean <matthew.wean@gmail.com>
2011-04-18 15:50:31 -0500

    Added CocoaDialog interface and finished basic renaming functionality with confirmation dialog

--- Music_Tag.rb
+++ Music_Tag.rb
@@ -3,7 +3,10 @@ require 'appscript'
 require 'OSAX'
 require 'Pattern'
 
+cocoaD = "./CocoaDialog.app/Contents/MacOS/CocoaDialog"
+
 sa = OSAX.osax
+genres_w_composers = ["classical", "ballet"]
 
 tracks = Appscript.app("iTunes").selection.get
 Appscript.app("iTunes").fixed_indexing.set(true)
@@ -33,15 +36,22 @@ tracks.each do |track|
   match_result << [pat_match, existing_tags]
 end
 
+
+
 i = 0
 changes = ""
 match_result.each do |mr|
   current = []
   newTags = []
   conflicts = []
+  if mr[0].has_key?(:composer) or mr[1].has_key?(:composer)
+    mr[0].delete_if {|k,v| k == :composer and !genres_w_composers.include?(v)}
+    mr[1].delete_if {|k,v| k == :composer and !genres_w_composers.include?(v)}
+  end
   mr[0].each {|a,b| current << "#{a}: #{b}"}
   mr[1].each { |a,b|  newTags << "#{a}: #{b}"}
-  mr[0].merge(mr[1]) {|key, oldV, newV| conflicts << "#{exstng_name[i]}\n\tExisting #{key}: #{oldV}, New: #{newV}"}
+  mr[0].merge(mr[1]) {|key, oldV, newV| conflicts << "#{exstng_name[i]}\n\tExisting #{key}: #{oldV}, New: #{newV}\n" unless oldV == newV}
+  # get longest string of current and .ljust to make into nice columns
   changes += exstng_name[i] + "\n"
   changes += "\tNew Tags: " + current.join(", ") + "\n"
   changes += "\tExisting tags: " + newTags.join(", ") + "\n"
@@ -53,10 +63,22 @@ match_result.each do |mr|
   i += 1
 end
 
-retag = sa.display_dialog("Pending changes:\n#{changes}\n\nCommit changes?", :buttons => ["No", "Yes"], :default_button => 2)[:button_returned]
+changeD = "#{changes}\nCommit changes?"
+changeD.gsub!(/([^A-Za-z0-9_\-.,:\/@\n])/n, "\\\\\\1").gsub!(/\n/, "'\n'")
+infText = "Review changes and click Yes to commit"
+infText.gsub!(/([^A-Za-z0-9_\-.,:\/@\n])/n, "\\\\\\1").gsub!(/\n/, "'\n'")
+args = "textbox --informative-text #{infText} --text #{changeD} --button1 'Yes' --button2 'No' --no-newline --title 'Pattern Matching Result'"
+retag = `#{cocoaD} #{args}`
 
-if retag = "Yes"
-  # commit changs
+if retag == "1"
+  i = 0
+  tracks.each do |track|
+   p mr_list = match_result[i][0].merge(match_result[i][1])
+    mr_list.each do |k,v|
+      track.send(k).set(v)
+    end
+    i += 1
+  end
 else
   exit
 end
